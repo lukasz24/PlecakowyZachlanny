55d588428b5974b48c9f08e119d4f467dcff7a10
lukasz24 <lukasz.pudzisz@gmail.com>
2018-10-16 11:35:35 +0200

    First

---
+++ src/Zadanie1/Item.java
@@ -0,0 +1,28 @@
+package Zadanie1;
+
+public class Item {
+	int value;
+	int weight;
+	float ratio;
+	String name;
+	
+	public Item(int value, int weight, String name) {
+		this.value = value;
+		this.weight = weight;
+		this.ratio = (float) value/weight;
+		this.name = name;
+	}
+	
+	public int getValue() {
+		return value;
+	}	
+	public int getWeight() {
+		return weight;
+	}	
+	public float getRatio() {
+		return ratio;
+	}
+	public String getName() {
+		return name;
+	}	
+}
---
+++ src/Zadanie1/Knapsack.java
@@ -0,0 +1,52 @@
+package Zadanie1;
+
+import java.util.LinkedList;
+
+public class Knapsack {
+	private int knapsackCapacity;
+	private LinkedList<Item> knapsackContent = new LinkedList<Item>();
+	
+	public Knapsack(int knapsackCapacity) {
+		this.knapsackCapacity = knapsackCapacity;		
+	}
+
+	public int getItemsWeight() {
+		int itemsWeight = 0;
+		for(Item p : knapsackContent) {
+			itemsWeight += p.getWeight();
+		}
+		return itemsWeight;
+	}
+	
+	public int getKnapsackValue() {
+		int value = 0;
+		for(Item p : knapsackContent) {
+			value += p.getValue();
+		}
+		return value;
+	}
+	
+	public int getFreeSpace() {
+		return knapsackCapacity - getItemsWeight();
+	}
+
+	public void addItem(Item item) {
+		knapsackContent.add(item);
+	}
+
+	public int getKnapsackCapacity() {
+		return knapsackCapacity;
+	}
+
+	public void setKnapsackCapacity(int knapsackCapacity) {
+		this.knapsackCapacity = knapsackCapacity;
+	}
+	
+	public void  printContent() {
+		System.out.println("\n####### CONTENT: #######\n");
+		for(Item p : knapsackContent) {
+			System.out.println(p.name + " Waga: " + p.getWeight() + " Wartoœæ: " + p.getValue());
+		}
+	}
+	
+}
